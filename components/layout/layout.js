0c5a8279752f240756d6a0930b59aaa5eaa60521
David Perelman <dperelman3@gmail.com>
2023-03-17 02:17:14 +0200

    Adding Layout & Navigation

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+
+import MainNavigation from './main-navigation';
+
+function Layout(props) {
+  <Fragment>
+    <MainNavigation />
+    <main>{props.children}</main>
+  </Fragment>;
+}
+
+export default Layout;
